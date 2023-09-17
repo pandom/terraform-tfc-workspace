cc4e9c221b66bd40198ea4a15df35ae4d4612d97
pandom <pandom@users.noreply.github.com>
2023-09-17 19:44:29 +1000

    Initial Commit

--- main.tf
+++ main.tf
@@ -1,1 +1,30 @@
+data "tfe_organization" "this" {
+  name = var.organization
+}
+## Use specific project if defined else place into the Default Project
+data "tfe_project" "this" {
+ name = (var.project_name != "" ? var.project_name : "Default Project" )
+}
+## Workspace configuration
+resource "tfe_workspace" "this" {
+  name = "${var.workspace_name}-${var.workspace_env}"
+  description = var.workspace_description
+  project_id = data.tfe_project.this.id
+  terraform_version = var.tf_version
+  assessments_enabled = var.workspace_assessments
+  auto_apply = var.workspace_autoapply
+  #tag_names = [ "${var.project_name}-${var.workspace_env}" ]
+}
 
+
+// Notification to be sent to service now
+// Eventually need to do a map here if prod is true, else to define diffences
+
+resource "tfe_notification_configuration" "this" {
+  name             = "${var.workspace_name}-${var.workspace_env}-notification"
+  enabled          = true
+  destination_type = "generic"
+  triggers         = ["run:errored","run:needs_attention","assessment:failed", "assessment:check_failure", "assessment:drifted"]
+  url              = "https://webhook.site/89dfd3a6-b541-4858-8322-89ab10d8f1aa"
+  workspace_id     = tfe_workspace.this.id
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,63 @@
+variable "organization" {
+    description = "Name of Terraform Organization"
+    default = ""
+    type = string
+}
 
+variable "workspace_description" {
+  description = "This is the workspace description"
+  default = "<< This workspace is managed via Terraform >>"
+  type = string
+}
+
+# variable "workspace_name" {
+#     description = "Name of Workspace to be created"
+#     default = ""
+#     type = string
+# }
+variable "workspace_name" {
+  default = 00000
+  description = "Workspace name as an Application ID"
+  type = number
+  validation {
+    # regex(...) fails if there is not 5 values
+    condition     = can(regex("^\\d{5}$", var.workspace_name))
+    error_message = "The Application ID used for workspace name must be 5 numbers"
+  }
+}
+variable "workspace_env" {
+  default = "dev"
+  description = "Workspace Environment name"
+  type = string
+  validation {
+    # Validation fails if it is not prod or dev
+    condition     = contains(["prod","dev"],var.workspace_env)
+    error_message = "The workspace environment used must be prod or dev"
+  }
+}
+variable "workspace_autoapply" {
+  description = "Enable autoapply on workspace"
+  default = true
+  type = bool
+}
+
+variable "workspace_assessments" {
+  description = "Enable Health and Drift Detection"
+  default = true
+  type = bool
+}
+variable "tf_beta" {
+  description = "Beta Support for Terraform"
+  default = false
+  type = bool
+}
+variable "tf_version" {
+  description = "Version of Terraform for workspace"
+  default = "latest"
+  type = string
+}
+variable "project_name" {
+  description = "AppID for Project Name"
+  default = ""
+  type = string
+}
